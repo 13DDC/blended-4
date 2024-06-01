bee28bdb7e21283ac78a76fcd82ec2962b321261
AliaNord <alia.nord.adventurer@gmail.com>
2024-06-01 14:21:02 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import './styles/normalize.css';
 import './styles/index.css';
 import { getAllProducts } from './requests/products';
-import {allProducts} from "./refs"
-getAllProducts();
-async function renderAllProducts ()
+import { refs } from './refs';
+import { createAllProductsMarkup } from './services/markupService';
+async function renderAllProducts() {
+  try {
+    const result = await getAllProducts();
+    const markup = createAllProductsMarkup(result.products);
+    refs.allProducts.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+renderAllProducts();
