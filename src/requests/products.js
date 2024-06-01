2c32be85609e9d67a62e6e3e905d0b17cdb1e31f
iahub4metra <buhai.artem7@gmail.com>
2024-06-01 14:15:47 +0100

    Update products.js

--- src/requests/products.js
+++ src/requests/products.js
@@ -4,3 +4,8 @@ export async function getAllProducts() {
   const result = await apiInstance.get('/products');
   return result.data;
 }
+
+export const addProducts = async (data) => {
+  const result = await apiInstance.post('/products/add', data)
+  return result.data
+}
